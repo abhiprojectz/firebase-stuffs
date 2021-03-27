4bcac271849b4b3292755e2161a39bcf61102370
abhiprojectz <abhiprojectz01@gmail.com>
2021-03-27 21:48:27 +0530

    adding files

---
+++ README.md
@@ -0,0 +1,20 @@
+This is our how our data is structured.
+
+![](ss/flow-new.PNG)
+
+This is all admin screens
+
+![](ss/ss2.PNG)
+
+This is specific admin's screen
+
+![](ss/ss3.PNG)
+
+the screen color changes accordingly 
+
++ if screen is unlocked color will be green
++ if screen is locked color will be red
+
+there will be errors if you found some then pls open issues.
+
+will update......
---
+++ config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDYAkT4DRnH1J-uqy7k3Zv1lLoZW--hop0",
+    authDomain: "teqno-7cccc.firebaseapp.com",
+    projectId: "teqno-7cccc",
+    storageBucket: "teqno-7cccc.appspot.com",
+    messagingSenderId: "782634594419",
+    appId: "1:782634594419:web:896dd04e8708833dcf7a06",
+    measurementId: "G-963NTQYQZF"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
